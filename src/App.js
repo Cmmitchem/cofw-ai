5d2de067ef534bd2b0395d85792db7844ae99131
Cmmitchem <c.m.mitchem@tcu.edu>
2024-03-31 14:26:27 -0500

    added dependent files and pdf renderer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+//import { Amplify } from 'aws-amplify';
+//import amplifyconfig from './amplifyconfiguration.json';
+//Amplify.configure(amplifyconfig);
+
+import { useState } from 'react';
+import PDFViewer from './pdfInput';
 
 function App() {
+  const [prompt, setPrompt] = useState('')
+
+  const handleChange = e => {
+    setPrompt(e.target.value);
+  }
+
+  const handleClick = e => {
+    e.preventDefault();
+    console.log(prompt);
+  }
+
+  
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Insert Prompt Below: 
         </p>
+
+        <input 
+          className='text-input'
+          type="text"
+          name="instruction"
+          onChange={handleChange} 
+          value={prompt}
+        />
+        <PDFViewer className='file-input'></PDFViewer>
+
+        <button className='button'onClick={handleClick}> Generate Response</button>
+        <div>
+
+        </div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="City of Fort Worth"
+          href="https://www.fortworthtexas.gov/Home"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          City of Fort Worth
         </a>
       </header>
     </div>
