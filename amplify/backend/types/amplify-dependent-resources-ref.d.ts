ba89bad4073f13b9e3451eaef010f11a5e4cd266
Cmmitchem <c.m.mitchem@tcu.edu>
2024-03-31 16:56:16 -0500

    connected backend, graphQL API, and S3 Storage for file upload and reference

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "cofwai": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "cofwaid9e6378c": {
       "AppClientID": "string",
@@ -8,6 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "platstorage": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
